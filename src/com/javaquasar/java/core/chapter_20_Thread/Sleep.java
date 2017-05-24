23b6ba1636bf0a61e496ac2f9ead4373263ef7ea
Java Quasar <java.quasar@gmail.com>
2017-05-24 20:25:05 +0300

    new chapters

---
+++ src/com/javaquasar/java/core/chapter_20_Thread/Sleep.java
@@ -0,0 +1,16 @@
+package com.javaquasar.java.core.chapter_20_Thread;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by artur on 24.05.17.
+ */
+//На данный момент рекомендуется использовать не Thread.sleep, а TimeUnit
+public class Sleep {
+    public static void main(String[] args) throws InterruptedException {
+        for(int i = 1; i < 60; i++) {
+            System.out.println(i);
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+}
